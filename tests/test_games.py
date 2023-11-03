122b6ae895c539314552b2aa15ee0a89f02a0855
Vinayak Prataap <prataap.vinayak@gmail.com>
2023-11-03 14:31:08 -0400

    Add tests for Tuesday

--- tests/test_games.py
+++ tests/test_games.py
@@ -6,3 +6,8 @@ def test_all_games(client):
     response = client.get("/api/games/")
     games = json.loads(response.data)
     assert games == database.games.to_dict(orient="records")
+
+def test_genres(client):
+    response = client.get("/api/games/genres")
+    genres = json.loads(response.data)['genres']
+    assert genres == list(database.games.genre.unique())
