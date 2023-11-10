ff3b275eff45cb42d51fd55938e31c760cade458
Vinayak Prataap <prataap.vinayak@gmail.com>
2023-11-10 09:50:50 -0500

    Revert genre test

--- tests/test_games.py
+++ tests/test_games.py
@@ -6,8 +6,3 @@ def test_all_games(client):
     response = client.get("/api/games/")
     games = json.loads(response.data)
     assert games == database.games.to_dict(orient="records")
-
-def test_genres(client):
-    response = client.get("/api/games/genres")
-    genres = json.loads(response.data)['genres']
-    assert genres == list(database.games.genre.unique())
