3412988f44c6611a8410ec65b13685b1eb5e5f68
Vinayak Prataap <prataap.vinayak@gmail.com>
2023-11-10 09:47:02 -0500

    Revert add tests

--- tests/test_books.py
+++ tests/test_books.py
@@ -6,17 +6,3 @@ def test_all_books(client):
     response = client.get("/api/books/")
     books = json.loads(response.data)
     assert books == database.books.to_dict(orient="records")
-
-
-def test_recommended_books(client):
-    response = client.get("/api/books/recommended")
-    books = json.loads(response.data)
-    for book in books:
-        assert book['average_rating'] > 4.5
-        assert book['ratings_count'] > 10000
-
-
-def test_all_publishers(client):
-    response = client.get("/api/books/publishers")
-    publishers = json.loads(response.data)['publishers']
-    assert publishers == list(database.books.publisher.unique())
